d8cce25b1ee6e82757eca22cc0c8e4268010a6b3
tahmeedr13 <tahmeedr13@gmail.com>
2025-10-25 18:34:31 -0400

    feat: add Circle and Triangle files

---
+++ circle.java
@@ -0,0 +1,25 @@
+public class Circle {
+    private Point center;
+    private double radius;
+
+    public Circle(Point center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
